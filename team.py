22d0d6356c30c8b64e0a58f289c76cc8856f7b9a
22MANAV22 <manavchhouda22@gmail.com>
2026-03-16 18:58:13 +0530

    Changed function farewell

--- team.py
+++ team.py
@@ -2,4 +2,4 @@ def greet():
     print("Hello amazing developers! Welcome to the project team!")
 
 def farewell():
-    print("Goodbye and see you next time")
+    print("Goodbye and see you next time.  ")
